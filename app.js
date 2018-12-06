cd57d4186801a97195f782bd8d154a4d0cf6f9d9
filippo <illiserf@gmail.com>
2018-12-06 12:23:53 +0100

    popeye-server cleaned

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ require('dotenv').config();
 const bodyParser   = require('body-parser');
 const cookieParser = require('cookie-parser');
 const express      = require('express');
-const favicon      = require('serve-favicon');
-const hbs          = require('hbs');
 const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
+const cors         = require('cors');
+const session      = require('express-session');
+const MongoStore   = require("connect-mongo")(session);
+const passport = require("passport");
 
+require("./config/passport-setup.js");
 
 mongoose
   .connect('mongodb://localhost/popeye-server', {useNewUrlParser: true})
@@ -19,9 +22,6 @@ mongoose
     console.error('Error connecting to mongo', err)
   });
 
-const app_name = require('./package.json').name;
-const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
-
 const app = express();
 
 // Middleware Setup
@@ -29,30 +29,30 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
-// Express View engine setup
+// Allow Cross-origin Resource sharing(CORS)
+//(allow access to the API from th frontend JS on a different domain/origin)
+app.use(
+  cors({
+    credentials: true,
+    //the array of domains/origins we want cookies from (just the React app)
+    origin: ["http://localhost:3000"]
+  })
+);
+app.use(
+  session({
+    secret: "y$TmzX8e7K409/j](jdNhyjqOxQ2wnq*#Lznn9B|Vr",
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use(require('node-sass-middleware')({
-  src:  path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
-      
-
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
-
-
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
-
-
-const index = require('./routes/index');
-app.use('/', index);
-
+//connection to the auth-router.js file , if I need more routes I can do like that
+const authRouter= require("./routes/auth-router.js");
+app.use("/api", authRouter);
 
 module.exports = app;
