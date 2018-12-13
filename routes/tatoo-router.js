d192c75de00580948edf5a975363a48d827e3e0b
Niccolò Boscolo <niccolo.boscolo88@icloud.com>
2018-12-13 15:57:32 +0100

    profile page ready

--- routes/tatoo-router.js
+++ routes/tatoo-router.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const TatooMaker = require("../models/tatoo-maker-model");
-const Appointment = require("../models/appointment-model")
+const Appointment = require("../models/appointment-model");
 const router = express.Router();
 
 //Get /tattoistList -Retrieve the list of Tattoist
@@ -14,6 +14,16 @@ router.get("/tattoistList", (req, res, next) => {
 
 // Get /tattoist/:id -Retrieve the details of One Tattoist
 router.get("/tattoistList/:id", (req, res, next) => {
+  const { id } = req.params;
+  console.log(id);
+  TatooMaker.findById(id)
+    .then(tatooMakerResults => {
+      console.log(tatooMakerResults);
+      res.json(tatooMakerResults);
+    })
+    .catch(err => next(err));
+});
+router.get("/tattoist/:id", (req, res, next) => {
   const { id } = req.params;
   console.log(id);
   TatooMaker.findById(id)
@@ -41,33 +51,32 @@ router.post("/tattoistlist", (req, res, next) => {
         $maxDistance: 10000 // 10km
       }
     }
-  }).populate("appointement")
+  })
+    .populate("appointement")
     .then(response => res.json(response))
     .catch(err => next(err));
 });
 
-
-router.post("/eventcreated/:tattoistId", (req, res,next)=>{
-  const {startDate, endDate, title}= req.body;
-  const {tattoistId}= req.params;
+router.post("/eventcreated/:tattoistId", (req, res, next) => {
+  const { startDate, endDate, title } = req.body;
+  const { tattoistId } = req.params;
   let clientId;
-  if (req.user.surname){
-    clientId=req.user._id;
+  if (req.user.surname) {
+    clientId = req.user._id;
   }
 
-  Appointment.create({startDate, endDate,title,clientId,tattoistId })
-  .then(appointmentDoc => {
-    console.log("appointmentDoc cote back", appointmentDoc)
-    res.json(appointmentDoc)
-  })
-  .catch(err => next(err))
-})
+  Appointment.create({ startDate, endDate, title, clientId, tattoistId })
+    .then(appointmentDoc => {
+      console.log("appointmentDoc cote back", appointmentDoc);
+      res.json(appointmentDoc);
+    })
+    .catch(err => next(err));
+});
 
-
-router.get("/appointments/:tattoistId", (req, res, next)=>{
-  const {tattoistId} = req.params;
-  Appointment.find({tattoistId: tattoistId})
-  .then(appointmentsArray => res.json({appointmentsArray}))
-  .catch(err => next(err))
-})
+router.get("/appointments/:tattoistId", (req, res, next) => {
+  const { tattoistId } = req.params;
+  Appointment.find({ tattoistId: tattoistId })
+    .then(appointmentsArray => res.json({ appointmentsArray }))
+    .catch(err => next(err));
+});
 module.exports = router;
