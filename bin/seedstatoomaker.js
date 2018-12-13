3c5214fc741b853da0dbf821f54db1ca08f3e5c5
Niccolò Boscolo <niccolo.boscolo88@icloud.com>
2018-12-13 13:07:27 +0100

    ready

--- bin/seedstatoomaker.js
+++ bin/seedstatoomaker.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const tattoist = require("../models/tatoo-maker-model.js");
 
-
 mongoose
   .connect(
     "mongodb://localhost/popeye-server",
@@ -20,106 +19,106 @@ mongoose
 
 const tattoistData = [
   {
-    picture: "../images/profile-pictures/1.jpg",
+    picture: "/images/profile-pictures/1.jpg",
     email: "hey64@hey.com",
     fullName: "Jean-Michel Apeuprès",
     adress: "40 rue du Colisée",
     city: "Paris",
-    appointement:["5c10dd56cdb2ba1735cc9804"],
+    appointement: ["5c10dd56cdb2ba1735cc9804"],
     geometry: { type: "Point", coordinates: [2.3110797, 48.871747] },
     description:
       "I am an incredible tattoist. I trainded alot on Filippo's back and draw all the plan of his jail and guess what!? he escaped! ",
     portfolio: [
-      "../images/tattoist-portfolio-images/1.jpg",
-      "../images/tattoist-portfolio-images/2.jpg",
-      "../images/tattoist-portfolio-images/3.jpeg",
-      "../images/tattoist-portfolio-images/4.jpg",
-      "../images/tattoist-portfolio-images/5.png"
+      "/images/tattoist-portfolio-images/1.jpg",
+      "/images/tattoist-portfolio-images/2.jpg",
+      "/images/tattoist-portfolio-images/3.jpeg",
+      "/images/tattoist-portfolio-images/4.jpg",
+      "/images/tattoist-portfolio-images/5.png"
     ]
   },
 
   {
-    picture: "../images/profile-pictures/2.jpg",
+    picture: "/images/profile-pictures/2.jpg",
     fullName: "Roger Rabbit",
     email: "hey1@hey.com",
     adress: "100 avenue Victor Hugo",
     city: "Paris",
-    appointement:["5c10dd56cdb2ba1735cc9804"],
+    appointement: ["5c10dd56cdb2ba1735cc9804"],
     geometry: { type: "Point", coordinates: [2.2829811999999947, 48.8688963] },
     description:
       "Specialized in colorful, geometric pieces that she's expanded into temporary tattoos, pins and other merch. ",
     portfolio: [
-      "../images/tattoist-portfolio-images/6.jpg",
-      "../images/tattoist-portfolio-images/7.jpg",
-      "../images/tattoist-portfolio-images/8.jpg",
-      "../images/tattoist-portfolio-images/9.jpg",
-      "../images/tattoist-portfolio-images/10.png"
+      "/images/tattoist-portfolio-images/6.jpg",
+      "/images/tattoist-portfolio-images/7.jpg",
+      "/images/tattoist-portfolio-images/8.jpg",
+      "/images/tattoist-portfolio-images/9.jpg",
+      "/images/tattoist-portfolio-images/10.png"
     ]
   },
 
   {
-    picture: "../images/profile-pictures/3.png",
+    picture: "/images/profile-pictures/3.png",
     fullName: "Francis Lenorman",
     email: "hey3@hey.com",
     adress: "17 rue Lafayette",
     city: "Paris",
-    appointement:["5c10dd56cdb2ba1735cc9804"],
+    appointement: ["5c10dd56cdb2ba1735cc9804"],
     geometry: { type: "Point", coordinates: [2.3364444999999705, 48.8740418] },
     description:
       "My beautiful graphic, all-black pieces evoke the mystery and seduction of film noir ",
     portfolio: [
-      "../images/tattoist-portfolio-images/11.jpg",
-      "../images/tattoist-portfolio-images/12.png",
-      "../images/tattoist-portfolio-images/13.jpg",
-      "../images/tattoist-portfolio-images/14.jpg",
-      "../images/tattoist-portfolio-images/15.jpg"
+      "/images/tattoist-portfolio-images/11.jpg",
+      "/images/tattoist-portfolio-images/12.png",
+      "/images/tattoist-portfolio-images/13.jpg",
+      "/images/tattoist-portfolio-images/14.jpg",
+      "/images/tattoist-portfolio-images/15.jpg"
     ]
   },
 
   {
-    picture: "../images/profile-pictures/4.jpeg",
+    picture: "/images/profile-pictures/4.jpeg",
     fullName: "Jean-Patrick Blah",
     adress: "14 rue du Parc Royal ",
     email: "hey4@hey.com",
     city: "Paris",
-    appointement:["5c10dd56cdb2ba1735cc9804"],
+    appointement: ["5c10dd56cdb2ba1735cc9804"],
     geometry: { type: "Point", coordinates: [2.3626663000000008, 48.8740418] },
     description:
       "My signature watercolor-inspired pieces look like they belong in a museum. ",
     portfolio: [
-      "../images/tattoist-portfolio-images/16.jpg",
-      "../images/tattoist-portfolio-images/17.png",
-      "../images/tattoist-portfolio-images/18.png",
-      "../images/tattoist-portfolio-images/19.jpg",
-      "../images/tattoist-portfolio-images/20.jpg"
+      "/images/tattoist-portfolio-images/16.jpg",
+      "/images/tattoist-portfolio-images/17.png",
+      "/images/tattoist-portfolio-images/18.png",
+      "/images/tattoist-portfolio-images/19.jpg",
+      "/images/tattoist-portfolio-images/20.jpg"
     ]
   },
 
   {
-    picture: "../images/profile-pictures/5.jpg",
+    picture: "/images/profile-pictures/5.jpg",
     fullName: "Pascal Ladale",
     adress: "12 rue de Tolbiac",
     email: "hey5@hey.com",
     city: "Paris",
-    appointement:["5c10dd56cdb2ba1735cc9804"],
+    appointement: ["5c10dd56cdb2ba1735cc9804"],
     geometry: { type: "Point", coordinates: [2.3364444999999705, 48.8740418] },
     description:
       "Paris-based and have  inked the likes of Robert Downey Jr. and Courtney Love. ",
     portfolio: [
-      "../images/tattoist-portfolio-images/21.jpg",
-      "../images/tattoist-portfolio-images/22.jpg",
-      "../images/tattoist-portfolio-images/23.jpeg",
-      "../images/tattoist-portfolio-images/24.jpg",
-      "../images/tattoist-portfolio-images/25.png"
+      "/images/tattoist-portfolio-images/21.jpg",
+      "/images/tattoist-portfolio-images/22.jpg",
+      "/images/tattoist-portfolio-images/23.jpeg",
+      "/images/tattoist-portfolio-images/24.jpg",
+      "/images/tattoist-portfolio-images/25.png"
     ]
   },
 
   {
-    picture: "../images/profile-pictures/6.jpg",
+    picture: "/images/profile-pictures/6.jpg",
     fullName: "Eleonor Marolex",
     adress: "20 avenue Niel",
     city: "Paris",
-    appointement:["5c10dd56cdb2ba1735cc9804"],
+    appointement: ["5c10dd56cdb2ba1735cc9804"],
     email: "hey6@hey.com",
     geometry: {
       type: "Point",
@@ -137,12 +136,12 @@ const tattoistData = [
   },
 
   {
-    picture: "./images/profile-pictures/7.jpg",
+    picture: "/images/profile-pictures/7.jpg",
     fullName: "Justine Titegoutte",
     adress: "8 rue de Didot",
     email: "hey7@hey.com",
     city: "Paris",
-    appointement:["5c10dd56cdb2ba1735cc9804"],
+    appointement: ["5c10dd56cdb2ba1735cc9804"],
     geometry: { type: "Point", coordinates: [2.3212078999999903, 48.8335874] },
     description:
       "I'm one of the most in-demand tattoo artists in Paris. He's inked the likes of Drake (famously, a prayer hand emoji) and Rihanna. According to the New York Times, his signature is a grouping of circles, arrows and dots, which fans have dubbed 'Woo arrows.' ",
@@ -156,11 +155,11 @@ const tattoistData = [
   },
 
   {
-    picture: "./images/profile-pictures/8.jpg",
+    picture: "/images/profile-pictures/8.jpg",
     email: "hey8@hey.com",
     fullName: "René Lataupe",
     adress: "2 rue de Pommard",
-    appointement:["5c10dd56cdb2ba1735cc9804"],
+    appointement: ["5c10dd56cdb2ba1735cc9804"],
     city: "Paris",
     geometry: { type: "Point", coordinates: [2.3853696999999556, 48.8355519] },
     description:
@@ -181,4 +180,3 @@ tattoist
     console.log("It works");
   })
   .catch(err => console.log(err));
-
